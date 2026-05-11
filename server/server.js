2b60f3e528c811f2928fb0908481caaa04005861
unknown <amrutkarsatyam@gmail.com>
2026-05-11 21:55:55 +0530

    for deployment

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ connectDB();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: process.env.CLIENT_URL && "http://localhost:5173",
     credentials: true,
   })
 );
